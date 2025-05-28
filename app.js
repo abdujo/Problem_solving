7f1bca62194a2e379805d7658aea2e0b63339f21
Kojo <kabdujo@gmail.com>
2025-05-28 14:54:14 -0400

    updated

--- app.js
+++ app.js
@@ -197,11 +197,179 @@
 
 
 
-function arrSum(arr) {
-    let sum = 0;
-    for  (let i = 0; i < arr.length; ++i) {
-        sum = sum + arr[i];
-    }
-    return sum;
+// function arrSum(arr) {
+//     let sum = 0;
+//     for  (let i = 0; i < arr.length; ++i) {
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+// }
+// console.log(arrSum([0, -5, -10]));
+
+// // Add up number from a signle number
+
+// function progressivesum(num) {
+
+//     let sum = 0;
+//     for  (let i = 1; i <= num; ++i) {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+// console.log(progressivesum(60));
+
+
+// Calculate the time,given a number in seconds return this number in mm:ss
+
+// function CalcTime(seconds) {
+
+//     let timerMinutes = Math.floor(seconds / 60);
+
+//     let timerSeconds = seconds % 60;
+
+//     if (timerMinutes.toString().length === 1) 
+//         {
+//         timerMinutes = '0' + timerMinutes
+//     }
+
+//     return timerMinutes + ":" + timerSeconds;
+
+
+// }
+
+// console.log(CalcTime(500));
+
+
+// find the largest number in an array
+
+
+// function getMax(arr) {
+//     let max = arr[0];
+
+//    for (let i = 1; i < arr.length; ++i) {
+
+//     if (arr[i] > max ) {
+//         max = arr[i];
+//     }
+
+
+//    }
+//    return max;
+
+// }
+
+// console.log(getMax([500, 400, 750]));
+
+// Reverse a string (given a string, retun the reversed string)
+
+// function reverseString(str) {
+//     let reverseString = ''
+//     for ( let i = 0; i < str.length; ++i) {
+
+
+//         reverseString = str[i] + reverseString;
+//     }
+//     return reverseString;
+
+   
+// }
+
+// console.log(reverseString('Thanks be to God almighty'));
+
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+   
+    
+//     }
+  
+// console.log(reverseString('I love programming'));
+
+// Turn every element in an array into 0
+
+
+// function ConvertToZeros(arr) {
+
+//   let  newArr = [];
+   
+//     for (let i = 0; i < arr.length; ++i) {
+
+//         newArr[i] = 0;
+//     }
+//     return newArr;
+    
+//     }
+  
+// console.log(ConvertToZeros([5, 100, 0]));
+
+
+// function ConvertToZeros(arr) {
+// return arr.map(elem => 0)
+    
+// }
+ 
+    
+  
+// console.log(ConvertToZeros([1, 2, 3, 4, 5, 6]));
+
+
+
+
+// filter out all the apples
+
+// function removeApples(arr) {
+//     return arr.filter(elem => elem !== 'apple');
+// }
+// console.log(removeApples(['apple', 'banana', 'orange', 'apple', 'grape']));
+
+
+// function removeApples(arr) {
+//    for (let i = 0; i < arr.length; ++i) {
+//         if (arr[i] === 'apple') {
+//             arr.splice(i, 1);
+//             i--; // Adjust index after removal
+//         }
+//     }
+//     return arr;
+// }
+// console.log(removeApples(['apple', 'banana', 'orange', 'apple', 'grape']));
+
+
+// function removeApples(arr) {
+//     let noApples = [];
+//     for (let i = 0; i < arr.length; ++i) {
+//         if (arr[i] !== 'apple') {
+//             noApples.push(arr[i]);
+//         }
+//     }
+//     return noApples;    
+// }
+// console.log(removeApples(['apple', 'banana', 'orange', 'apple', 'grape']));
+
+
+// Filter Out all the falsy values
+
+// function filterOutFalsy(arr) {
+//     return arr.filter(elem => !!elem === true);
+// }
+// console.log(filterOutFalsy(["",[], "apple", 0, null, undefined,"Banana" , "0"]));
+
+
+// function filterOutFalsy(arr) {
+//     let trutthyarr = [];
+
+//     for (let i = 0; i < arr.length; ++i) {
+//         if (!!arr[i] === true) {
+//             trutthyarr.push(arr[i]);
+//         }
+//     }
+//    return trutthyarr;
+// }
+// console.log(filterOutFalsy(["", "apple", 0, null, undefined,"Banana" ,[],  "0"]));
+
+// Truthy to true, Falsy to false
+
+function convertToBoolean(arr) {
+    return arr.map(elem => !!elem);
 }
-console.log(arrSum([0, -5, -10]));
+
+console.log(convertToBoolean([500, 0, "David", "", []]));
